08b0518e1c54e42d7808da6d6d52ad27decfa0bb
Micah Mosley <micah.mosley@utexas.edu>
2020-07-14 21:51:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   
   
   def add_item(title, price, quantity=1)
-    @items.push(title*quantity)
+    @items.push.split(title*quantity)
     self.total += price*quantity
   end 
   
