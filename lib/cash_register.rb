a7a0dcb2bfdbb263d3713028fa299b229b8ea976
Micah Mosley <micah.mosley@utexas.edu>
2020-07-14 21:25:41 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,12 @@
 
 class CashRegister 
+  attr_accessor :total 
+  
   def initialization (discount=0)
     @total=0 
   end 
-  attr_accessor :total 
+  
   
   def add_item(item, price)
-    self.total+=price 
+    self.total +=price 
 end 
